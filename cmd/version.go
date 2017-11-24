3689f4909207dc06269245ec629a8831fb8670c8
Hoang Hoa <nobi.hoa@gmail.com>
2017-11-24 22:48:47 +0700

    Add command to return the current version

--- cmd/version.go
+++ cmd/version.go
@@ -11,12 +11,13 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// VERSION Hold the current version of this application
 const VERSION = "0.0.1"
 
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "Print the version.",
+	Short: "Print the version",
 	Long: `Print the version.`,
 	Aliases: []string{ "v" },
 	Example: `

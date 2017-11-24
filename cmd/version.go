edbee94c7c58d375a77f9f8b4bd340c51095b425
Hoang Hoa <nobi.hoa@gmail.com>
2017-11-24 09:22:55 +0700

    Init cobra app with version command

---
+++ cmd/version.go
@@ -0,0 +1,32 @@
+// MIT License
+// 
+// Copyright (c) 2017 Nobi Hoang <nobi.hoa@gmail.com>
+// 
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+const VERSION = "0.0.1"
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version.",
+	Long: `Print the version.`,
+	Aliases: []string{ "v" },
+	Example: `
+	mysh version
+	`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(VERSION)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
---
+++ main.go
@@ -0,0 +1,14 @@
+// MIT License
+// 
+// Copyright (c) 2017 Nobi Hoang <nobi.hoa@gmail.com>
+// 
+
+package main
+
+import (
+	"github.com/hoahm/mysh/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
